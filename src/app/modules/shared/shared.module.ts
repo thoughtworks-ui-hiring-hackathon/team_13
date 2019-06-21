1a5cf65ea98175a514d40803c4c4318aef098f7c
hardik <hpatel5@slb.com>
2019-06-21 13:23:24 +0530

    genre updated in movie card

--- src/app/modules/shared/shared.module.ts
+++ src/app/modules/shared/shared.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 import { MovieCardComponent } from './movie-card/movie-card.component';
 import { MaterialModule } from 'src/app/material.module';
 import { RatingComponent } from './rating/rating.component';
+import { GenrePipe } from './pipes/genre.pipe';
 
 @NgModule({
-  declarations: [MovieCardComponent, RatingComponent],
+  declarations: [MovieCardComponent, RatingComponent, GenrePipe],
   imports: [CommonModule, MaterialModule],
   exports: [MovieCardComponent, RatingComponent]
 })
--- src/app/services/movie.service.ts
+++ src/app/services/movie.service.ts
@@ -1,38 +1,67 @@
 import { Injectable } from '@angular/core';
 import Constant from '../../constant';
 import { HttpClient } from '@angular/common/http';
+import { Genre } from '../models/genre';
 
 @Injectable({
-  providedIn: 'root',
+  providedIn: 'root'
 })
 export class MovieService {
+  movieGenres: Genre[] = [];
+  constructor(private http: HttpClient) {
+    this.setUpMovieData();
+  }
 
-    constructor(private http: HttpClient) { }
+  setUpMovieData() {
+    this.getMovieGenres().subscribe((result: any) => {
+      this.movieGenres = result.genres;
+      console.log(this.movieGenres);
+    });
+  }
 
-    getLatestMovie(){
-        return this.http.get(`${Constant.base_url}/movie/now_playing?api_key=${Constant.API_KEY}&language=en-US&include_adult=false`);
-    }
+  getLatestMovie() {
+    return this.http.get(
+      `${Constant.base_url}/movie/now_playing?api_key=${
+        Constant.API_KEY
+      }&language=en-US&include_adult=false`
+    );
+  }
 
-    getTrendingMovie(){
-        return this.http.get(`${Constant.base_url}/trending/movie/week?api_key=${Constant.API_KEY}`);
-    }
+  getTrendingMovie() {
+    return this.http.get(
+      `${Constant.base_url}/trending/movie/week?api_key=${Constant.API_KEY}`
+    );
+  }
 
-    getMostWatchedMovie(){
-        return this.http.get(`${Constant.base_url}/movie/popular?api_key=${Constant.API_KEY}&language=en-US&page=1`);
-    }
+  getMostWatchedMovie() {
+    return this.http.get(
+      `${Constant.base_url}/movie/popular?api_key=${
+        Constant.API_KEY
+      }&language=en-US&page=1`
+    );
+  }
 
-    getMovieGenres(){
-        return this.http.get(`${Constant.base_url}/genre/movie/list?api_key=${Constant.API_KEY}&language=en-US`);    
-    }
+  getMovieGenres() {
+    return this.http.get(
+      `${Constant.base_url}/genre/movie/list?api_key=${
+        Constant.API_KEY
+      }&language=en-US`
+    );
+  }
 
-    getSimilarMovie(movieId: string){
-        return this.http.get(`${Constant.base_url}/movie/${movieId}/similar?api_key=${Constant.API_KEY}&language=en-US&page=1`);    
-    }
+  getSimilarMovie(movieId: string) {
+    return this.http.get(
+      `${Constant.base_url}/movie/${movieId}/similar?api_key=${
+        Constant.API_KEY
+      }&language=en-US&page=1`
+    );
+  }
 
-    getMovieSearch(searchQuery: string){
-        return this.http.get(`${Constant.base_url}/search/multi?api_key=${Constant.API_KEY}&language=en-US&query=${searchQuery}&page=1&include_adult=false`);    
-    }
-
-
-
+  getMovieSearch(searchQuery: string) {
+    return this.http.get(
+      `${Constant.base_url}/search/multi?api_key=${
+        Constant.API_KEY
+      }&language=en-US&query=${searchQuery}&page=1&include_adult=false`
+    );
+  }
 }
