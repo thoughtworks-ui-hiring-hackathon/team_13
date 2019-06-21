73d1a334e2bdfefa08822978670696b7f6213ce0
chirag patel <chiragpatel08bce050@gmail.com>
2019-06-21 17:11:31 +0530

    final change in explore page

--- src/app/modules/explore/explore.module.ts
+++ src/app/modules/explore/explore.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ExploreComponent } from './explore/explore.component';
 import { ExploreRoutingModule } from './explore-routing.module';
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
   declarations: [ExploreComponent],
-  imports: [CommonModule, ExploreRoutingModule]
+  imports: [CommonModule, ExploreRoutingModule,SharedModule]
 })
 export class ExploreModule {}
