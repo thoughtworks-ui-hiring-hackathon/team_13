289866e0704a762aa4189a8d86a008e95c13de48
hardik <hpatel5@slb.com>
2019-06-21 16:26:48 +0530

    removed api key

--- src/constant.ts
+++ src/constant.ts
@@ -1,5 +1,4 @@
-export default class Constant{
-    public static  API_KEY ="5448b3ea0657e9db72d1f701c4a199cb";
-    public static base_url ="https://api.themoviedb.org/3";
-
+export default class Constant {
+  public static API_KEY = '';
+  public static base_url = 'https://api.themoviedb.org/3';
 }
